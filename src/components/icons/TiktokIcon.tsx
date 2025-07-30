1120dd3ce5fedaeddbdb2ebb977054459d4181f8
carlplata <cplata2002@gmail.com>
2025-07-30 13:49:10 -0600

    Updated TiktokIcon.tsx

---
+++ src/components/icons/TiktokIcon.tsx
@@ -0,0 +1,20 @@
+// src/components/icons/TiktokIcon.tsx
+import React from 'react';
+
+export const TiktokIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M21 7v3.3a4.7 4.7 0 0 1-4.7 4.7h-1.6a2.7 2.7 0 0 1-2.7-2.7V8.7a2.7 2.7 0 0 0-2.7-2.7H5" />
+    <path d="M11 7v10.7a4.7 4.7 0 0 0 4.7 4.7h0a4.7 4.7 0 0 0 4.7-4.7V11" />
+  </svg>
+);
